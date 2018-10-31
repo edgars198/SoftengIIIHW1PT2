3efe32a44d78e700aba16560d57a2f4f6d519efb
Edgars <Edgars@192.168.1.13>
2018-10-31 15:21:07 +0000

    update

--- src/main/java/SourcePac/Driver.java
+++ src/main/java/SourcePac/Driver.java
@@ -1,16 +1,50 @@
 package SourcePac;
 import assignment1.Student;
-/**
- *
- * @author Edgars
- */
+import assignment1.Module;
+import assignment1.CourseProgramme;
+import java.util.*;
+
 public class Driver {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
-        // TODO code application logic here
+        //array list that contains students
+        ArrayList<Student> studentsTakingClass = new ArrayList();
+        ArrayList<Module> modulesAssociated = new ArrayList();
+        
+        //populating the arraylist
+        studentsTakingClass.add(new Student("Joe", 19, "19951106", 112));
+        studentsTakingClass.add(new Student("Sacy", 20, "19940806", 445));
+        studentsTakingClass.add(new Student("Bobby", 17, "19981205", 445));
+        studentsTakingClass.add(new Student("Debbie", 18, "19981202", 498));
+        
+        //creating new modules
+        Module softEngIII = new Module("Software Engineering III", "CT101");
+        Module classicsII = new Module("Classic Studies", "CL201");
+        Module calculus = new Module("Calculus", "CA444");
+        
+        //adding students to modules
+        softEngIII.addStudent(studentsTakingClass);
+        classicsII.addStudent(studentsTakingClass);
+        calculus.addStudent(studentsTakingClass);
+        
+        //creating an arts course
+        CourseProgramme arts = new CourseProgramme("Arts");
+        
+        //adding modules to arraylist
+        modulesAssociated.add(softEngIII);
+        modulesAssociated.add(classicsII);
+        modulesAssociated.add(calculus);
+        
+        //adding modules to course
+        arts.addModules(modulesAssociated);
+        
+        System.out.println(arts);
+        
+        
+        
+        
+        
+        
     }
     
 }
